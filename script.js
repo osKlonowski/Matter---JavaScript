14ec713c46fb17e472b4f42211c18b783a9add9a
Oskar Klonowski <oskarklonowski@Oskars-MacBook-Pro.local>
2018-08-13 16:34:02 +0200

    Adding local files to github

---
+++ script.js
@@ -0,0 +1,77 @@
+var Engine = Matter.Engine;
+var World = Matter.World;
+var Bodies = Matter.Bodies;
+
+var engine;
+var world;
+var particles= [];
+var p;
+var b;
+var plinkos = [];
+var bounds = [];
+var cols = 11;
+var rows = 10;
+
+
+function setup() {
+  createCanvas(600, 700);
+
+  // create an engine
+  engine = Engine.create();
+  world = engine.world;
+  world.gravity.y = 2;
+  newParticle();
+  var spacing = width / cols;
+  for (var j = 0; j < rows; j++) {
+    for (var i = 0; i < cols+1; i++) {
+      var x = i*spacing;
+      if (j % 2 == 0) {
+        x += spacing/2;
+      }
+      var y = spacing+j*spacing;
+      p = new Plinko(x, y, 4);
+      plinkos.push(p);
+    }
+  }
+
+  b = new Boundary(width/2, height+50, width, 100);
+  bounds.push(b);
+  for (var i = 0; i < cols+1; i++) {
+    var x = i * spacing - 10;
+    var h = 100;
+    var w = 10;
+    var y = height - h / 2;
+    b = new Boundary(x, y, w, h);
+    bounds.push(b);
+  }
+}
+
+function newParticle() {
+  p = new Particle(300, 0, 10);
+  particles.push(p);
+}
+
+function draw() {
+  if (frameCount % 8 == 0) {
+    newParticle();
+  }
+
+  background(51);
+  Engine.update(engine);
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].show();
+    if (particles[i].isOffScreen()) {
+      World.remove(world, particles[i].body);
+      particles.splice(i, 1);
+      i--;
+    }
+  }
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].show();
+  }
+  for (var i = 0; i < bounds.length; i++) {
+    bounds[i].show();
+  }
+
+
+}
